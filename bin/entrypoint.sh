8c1bd31f519f632db680c592c22bccd846d6036f
Jonathan Monette <jon.monette@gmail.com>
2023-07-24 00:24:18 -0500

    update output

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -7,7 +7,7 @@ git config --global user.name "Github Actions[bot]"
 currentYear=$(date +'%Y')
 branchName="license/${currentYear}"
 commitMessage="Update license to ${currentYear}"
-python3 /app/main.py --file $1 > $1
+python3 /app/main.py --file $1 --output $1
 git checkout "${branchName}" || git checkout -b "${branchName}"
 git add $1
 git commit --message "${commitMessage}}"
